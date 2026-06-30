fcdc816aaff00e4498b37a9b6b6ad313c8ec4ce7
KrinshsmithKava <krinshsmith.nivzen@gmail.com>
2026-06-30 17:18:33 +0530

    react

--- src/context/MovieRow.jsx
+++ src/context/MovieRow.jsx
@@ -1,8 +1,11 @@
 import React, { useContext } from 'react';
 import UserContext from './userContext';
+import CartContext from './cartContext';
 
 function MovieRow(props) {
     const userContext = useContext(UserContext);
+    const cartContext = useContext(CartContext);
+
 
     return (
         <div>
