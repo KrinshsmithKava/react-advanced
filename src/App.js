7ef13ddd75fcd1210b4df93e16ab26af10e2cdd8
KrinshsmithKava <krinshsmith.nivzen@gmail.com>
2026-06-30 15:40:38 +0530

    react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-import Movie from './hoc/Movie';
-import Counter from './hooks/Counter';
-import Users from './hooks/Users';
+import React, { Component } from 'react';
+import MoviePage from './context/MoviePage';
 
-function App() {
-  return (
-    <Users />
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MoviePage />
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+// import Movie from './hoc/Movie';
+// import Counter from './hooks/Counter';
+// import Users from './hooks/Users';
+
+// function App() {
+//   return (
+//     <Movie id={1} />
+//   );
+// }
+
+// export default App;
